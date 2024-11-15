ece66d9dddf36f119a6527881abf7ad94a21a92b
Ethan-net <koredeajibola@gmail.com>
2024-11-15 14:28:50 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Logo from './logo'
-import instagram from 'https://img.icons8.com/windows/32/instagram-new.png"'
 
 export default function Footer() {
   return (
@@ -8,7 +7,7 @@ export default function Footer() {
       <div className=''>
         <Logo/>
         <div>
-          <img src={instagram} alt=""/>
+         
         </div>
       </div>
     </div>
